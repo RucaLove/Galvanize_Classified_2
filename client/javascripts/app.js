f3ebfdafac30959eb7da3c97c68e692634fdf79c
Haley Kalb <haley.kalb@gmail.com>
2017-05-19 09:41:21 -0600

    stopping point

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,5 +1,4 @@
 (function() {
-  angular.module('classifieds', ['ui.router', 'angularMoment'])
   angular.module("classifieds", [])
   .component('ads', {
     controller: controller,
@@ -9,10 +8,6 @@
     templateUrl: '../views/ad-list.html',
     controller: adlistController
   })
-  // .component('addingAd', {
-  //   templateUrl: '../views/ad-list.html',
-  //   controller: addAdController
-  // })
 
   function controller(AdService){
   const vm = this;
@@ -23,11 +18,6 @@
       vm.ads = data
     })
   }
-
-  // vm.addAd = function() {
-  //   vm.ads.push(vm.ad)
-  //   delete vm.ad
-  // }
 }
 
   function adlistController(NewService) {
@@ -46,60 +36,4 @@
     }
   }
 
-  // function addAdController($http) {
-  //   const vm = this
-  //
-  //   vm.$onInit = onInit
-  //
-  //   function onInit() {
-  //     vm.addingAd = false
-  //   }
-  // }
-
 })()
-
-// (function() {
-//
-//   angular
-//     .module('classifieds', ['ngRoute'])
-//     .config(config);
-//
-//   function config($routeProvider, $locationProvider) {
-//     $routeProvider
-//       .when('/classifieds', {
-//         templateUrl: '../views/index.html',
-//         controller: 'ClassifiedsController',
-//         controllerAs: 'vm',
-//         resolve: {
-//           classifieds: getAllClassifieds
-//         }
-//       })
-//       // .when('/pirates/new', {
-//       //   templateUrl: '../views/pirates/new.html',
-//       //   controller: 'NewPirateController',
-//       //   controllerAs: 'vm'
-//       // })
-//       // .when('/pirates/:id/edit', {
-//       //   templateUrl: '../views/pirates/edit.html',
-//       //   controller: 'EditPirateController',
-//       //   controllerAs: 'vm',
-//       //   resolve: {
-//       //     pirate: getPirateById
-//       //   }
-//       // })
-//       .otherwise({ redirectTo: '/index' })
-//     $locationProvider.html5Mode(true);
-//   }
-//
-//   function getAllClassifieds(AdService) {
-//     return AdService.getAd();
-//   }
-//
-//   function getAdById(AdService, $route) {
-//     return AdService.getAd($route.current.params.id);
-//   }
-//
-//   config.$inject = ['$routeProvider', '$locationProvider'];
-//   getAllClassifieds.$inject = ['AdService'];
-//   getAdById.$inject = ['AdService', '$route'];
-// })()
